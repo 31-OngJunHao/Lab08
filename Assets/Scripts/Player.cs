3d049ea09cd7457c9bf29705bfe783aed8b679c5
31-OngJunHao <ongjunhao2609@gmail.com>
2022-10-26 10:41:12 +0800

    Added Unity File

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public int Score;
+
+    public Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score = ObstacleMovement.score;
+        ScoreText.text = "Score: " + Score;
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            //Gamelose
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
